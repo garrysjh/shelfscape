0eeaace0326612c07871e851400c89dd09a84535
garrysjh <garrysjh@gmail.com>
2024-10-20 14:24:41 +0800

    create initializer sql script

---
+++ mediplus-lite/scripts/shelfscape.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS
+User(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(50) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(50) NOT NULL,
+    `phone` VARCHAR(50) NOT NULL
+)
